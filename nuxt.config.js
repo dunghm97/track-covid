0f343c2e92c75c3cd9badb1c03fd96c146e98354
dunghoang97 <dunghoang.5397@gmail.com>
2021-06-15 15:06:43 +0700

    fix ico

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,9 +19,7 @@ export default {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { hid: 'description', name: 'description', content: '' },
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '@/assets/img/virus.ico' },
-    ],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/virus.ico' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
